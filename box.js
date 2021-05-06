39e4c36c03aab343b4b394b616e460544fe94b70
luckylovs <77058551+luckylovs@users.noreply.github.com>
2021-05-06 13:52:54 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,36 @@
+class Polygon{
+
+    constructor(x,y,r){
+    var options = {
+        isStatic:false,
+        'restitution':0.3,
+        'friction':0,
+        'density':1.2
+    }
+    this.x = x
+    this.y =y
+    this.r = r
+
+    this.image = loadImage("polygon.png");
+    this.body = Bodies.circle(50,200,20);
+       
+    World.add(world, this.body);
+   
+    }
+    
+    display(){
+    var pos = this.body.position;
+    push();
+    translate(pos.x, pos.y);
+    //rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.r,this.r);
+    pop();
+    
+    
+    }
+    }
+    
+    
+    
+    
